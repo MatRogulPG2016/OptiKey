6cdb468ac2077f9a92e76a01d7aa0d14104f2a46
Mateusz Rogulski <mateusz@rogulscy.pl>
2016-02-07 22:42:48 +0100

    Basic constructor's tests for PointServiceSource class

---
+++ src/JuliusSweetland.OptiKey.UnitTests/Observables/PointSources/PointServiceSourceTests.cs
@@ -0,0 +1,35 @@
+﻿using JuliusSweetland.OptiKey.Enums;
+using JuliusSweetland.OptiKey.Observables.PointSources;
+using JuliusSweetland.OptiKey.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace JuliusSweetland.OptiKey.UnitTests.Observables.PointSources
+{
+    [TestFixture]
+    public class PointServiceSourceTests
+    {
+        [Test]
+        public void IsStateRunningAfterCreate()
+        {
+            var pointTtl = new TimeSpan();
+            var mockPointGeneratingService = new Mock<IPointService>();
+
+            var pointServicePoint = new PointServiceSource(pointTtl, mockPointGeneratingService.Object);
+
+            Assert.AreEqual(pointServicePoint.State, RunningStates.Running);
+        }
+
+        [Test]
+        public void IsSequenceSettedAfterCreate()
+        {
+            var pointTtl = new TimeSpan();
+            var mockPointGeneratingService = new Mock<IPointService>();
+
+            var pointServicePoint = new PointServiceSource(pointTtl, mockPointGeneratingService.Object);
+
+            Assert.IsNotNull(pointServicePoint.Sequence);
+        }
+    }
+}
